c5e021d5e1124cbd71d07869678b71becf5cea49
Arnold <arnold.2003.06@gmail.com>
2025-10-05 03:30:45 -0500

    Sincronización, esta si

--- app/config.py
+++
@@ -1,5 +0,0 @@
-import os
-class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "dev-key-change-me")
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///app.db")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
